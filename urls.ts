5fd69742c5a884221d7504fa31ca3124b8fbe240
Tawonga021 <reginaldjongwe@gmail.com>
2026-05-09 23:10:30 +0200

    Update urls.ts

--- urls.ts
+++ urls.ts
@@ -1,5 +1,5 @@
 
-export const baseUrl:String=process.env.NODE_ENV === "development" ? "http://localhost:4000/api/v1/" : 'https://api.bidfirstauctions.co.zw/api/v1/';
-export const imageServerUrl:String=process.env.NODE_ENV === "development" ? "http://localhost:4000/" :'https://api.bidfirstauctions.co.zw/'
-export const serverUrl=process.env.NODE_ENV === "development" ? "http://localhost:4000/" :'https://api.bidfirstauctions.co.zw/'
+export const baseUrl:String=process.env.NODE_ENV === "development" ? "http://localhost:4000/api/v1/" : 'https://bidfest-api.onrender.com/api/v1/';
+export const imageServerUrl:String=process.env.NODE_ENV === "development" ? "http://localhost:4000/" :'https://bidfest-api.onrender.com' 
+export const serverUrl=process.env.NODE_ENV === "development" ? "http://localhost:4000/" :'https://bidfest-api.onrender.com/'
 export const bidfirstUrl:String= process.env.NODE_ENV ==="development" ? "http://localhost:3000" :'https://bidfirstauctions.co.zw'
